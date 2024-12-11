e80233c42645eb197ca6446c60cd6161c6973189
David Shapiro <daniek332@gmail.com>
2024-12-11 19:11:46 +0200

    Hiding (#6)
    * Player can hide
    
    * small fixes of interactable classes
    
    * bug fix, another bug in character switching popped up
    
    * sfalkdgjle (!)
    
    * ;lesfdjeaioj
    
    ---------
    
    Co-authored-by: yoavk-sudo <kenkendler123@gmail.com>

---
+++ Assets/Scripts/Interactables/InteractableObject.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public abstract class InteractableObject : MonoBehaviour
+{
+    protected PlayerCharacterController CachedPlayerController;
+
+    protected virtual void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out CachedPlayerController))
+        {
+            InformManagerOfInteractability();
+        }
+    }
+
+    protected virtual void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.TryGetComponent(out CachedPlayerController))
+        {
+            InformManagerOfInteractability();
+        }
+    }
+
+    public void InformManagerOfInteractability()
+    {
+        InteractablesManager.Instance.AddInteractableObject(this as IInteractable);
+    }
+}
---
+++ Assets/Scripts/Interactables/SwitchCharacter.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class SwitchCharacter : InteractableObject, IInteractable
+{
+    [SerializeField] GameObject CaptainObject;
+    [SerializeField] GameObject GirlObject;
+
+    [ContextMenu("Switch Characters")]
+    public void Interact()
+    {
+        ToggleCharactersActivity();
+    }
+
+    public void UnInteract()
+    {
+        ToggleCharactersActivity();
+    }
+
+    private void ToggleCharactersActivity()
+    {
+        CaptainObject.SetActive(!CaptainObject.activeSelf);
+        GirlObject.SetActive(!GirlObject.activeSelf);
+    }
+}
