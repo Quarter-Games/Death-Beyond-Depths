7b90f92c9e05c7d9515784118394086262102134
yoavk-sudo <kenkendler123@gmail.com>
2025-01-18 21:00:02 +0200

    animator

--- Assets/Scripts/Environment/FogOfWarController.cs
+++ Assets/Scripts/Environment/FogOfWarController.cs
@@ -6,18 +6,34 @@ using DG.Tweening.Plugins.Options;
 public class FogOfWarController : MonoBehaviour
 {
     [SerializeField] Material FogMaterial;
+    [SerializeField] Animator Animator;
     [SerializeField] float FadeSpeed;
+    [SerializeField] float FadedValue;
 
     float DefaultFogCoverage;
+    bool DefaultSide;
     bool IsOnTheRight = false;
     
-    private const string FOG_COVERAGE = "FogCoverage";
+    private const string FOG_COVERAGE_LEFT = "_FogCoverageLeft";
+    private const string FOG_COVERAGE_RIGHT = "_FogCoverageRight";
+    private const string SPRITE_MASK_MOVE_LEFT = "MovingLeft";
+    private const string SPRITE_MASK_MOVE_RIGHT = "MovingRight";
+    private const string IS_FOG_ON_LEFT = "_IsOnLeft";
+
+    private void OnDisable()
+    {
+        FogMaterial.SetFloat(FOG_COVERAGE_LEFT, DefaultFogCoverage);
+        FogMaterial.SetInt(IS_FOG_ON_LEFT, DefaultSide ? 1 : 0);
+    }
 
     private void Start()
     {
-        DefaultFogCoverage = FogMaterial.GetFloat(FOG_COVERAGE);
+        DefaultFogCoverage = FogMaterial.GetFloat(FOG_COVERAGE_LEFT);
+        IsOnTheRight = FogMaterial.GetInt(IS_FOG_ON_LEFT) == 1 ? true : false;
+        DefaultSide = IsOnTheRight;
     }
 
+    [ContextMenu("Fade")]
     public void DissolveAndMoveFog()
     {
         DissolveFog().OnComplete(() =>
@@ -28,10 +44,24 @@ public class FogOfWarController : MonoBehaviour
 
     private TweenerCore<float, float, FloatOptions> DissolveFog()
     {
+        string leftOrRight;
+        if (IsOnTheRight)
+        {
+            Animator.SetBool(SPRITE_MASK_MOVE_RIGHT, false);
+            Animator.SetBool(SPRITE_MASK_MOVE_LEFT, true);
+            leftOrRight = FOG_COVERAGE_RIGHT;
+        }
+        else
+        {
+            Animator.SetBool(SPRITE_MASK_MOVE_LEFT, false);
+            Animator.SetBool(SPRITE_MASK_MOVE_RIGHT, true);
+            leftOrRight = FOG_COVERAGE_LEFT;
+        }
+        FogMaterial.SetInt(IS_FOG_ON_LEFT, IsOnTheRight ? 1 : 0);
         return DOTween.To(
-                        () => FogMaterial.GetFloat(FOG_COVERAGE),
-                        value => FogMaterial.SetFloat(FOG_COVERAGE, value),
-                        0,
+                        () => FogMaterial.GetFloat(FOG_COVERAGE_LEFT),
+                        value => FogMaterial.SetFloat(leftOrRight, value),
+                        FadedValue,
                         FadeSpeed
                     );
     }
@@ -39,10 +69,12 @@ public class FogOfWarController : MonoBehaviour
     private void MoveFog()
     {
         IsOnTheRight = !IsOnTheRight;
+        //FogMaterial.SetFloat(FOG_COVERAGE,
     }
 
-    private void ResetFog()
+    private void EndAnimations()
     {
-        FogMaterial.SetFloat(FOG_COVERAGE, DefaultFogCoverage);
+        Animator.SetBool(SPRITE_MASK_MOVE_LEFT, false);
+        Animator.SetBool(SPRITE_MASK_MOVE_RIGHT, false);
     }
 }
