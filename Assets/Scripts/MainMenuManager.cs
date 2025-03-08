6844278cc069fa30a6e20440d6218b8a75f3dcee
dendefo <daniek332@gmail.com>
2025-03-08 20:07:36 +0200

    Loading

--- Assets/Scripts/MainMenuManager.cs
+++ Assets/Scripts/MainMenuManager.cs
@@ -1,11 +1,35 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
     [SerializeField] string MainSceneName = "MainScene";
+    [SerializeField] string LoadingSceneName = "LoadingScene";
     public void StartGame()
     {
-        SceneManager.LoadScene(MainSceneName);
+        StartCoroutine(OpenGameScene());
+        DontDestroyOnLoad(gameObject);
+    }
+    public IEnumerator OpenGameScene()
+    {
+        yield return new WaitForSeconds(1f);
+        var asyncLoading = SceneManager.LoadSceneAsync(LoadingSceneName);
+        asyncLoading.completed += AsyncLoading_completed;
+
+    }
+
+    private void AsyncLoading_completed(AsyncOperation obj)
+    {
+        obj.completed -= AsyncLoading_completed;
+        StartCoroutine(LoadMainScene());
+    }
+
+    private IEnumerator LoadMainScene()
+    {
+        yield return new WaitForSeconds(1f);
+        SceneManager.LoadSceneAsync(MainSceneName);
+        Destroy(gameObject);
     }
 }
