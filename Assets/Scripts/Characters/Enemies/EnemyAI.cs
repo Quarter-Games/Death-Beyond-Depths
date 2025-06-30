3b4502d01ec8872f5ce8c26f4151d152d0818d0a
yoavk-sudo <kenkendler123@gmail.com>
2025-06-30 15:35:40 +0300

    stuff, shadow enemy

--- Assets/Scripts/Characters/Enemies/EnemyAI.cs
+++ Assets/Scripts/Characters/Enemies/EnemyAI.cs
@@ -4,7 +4,6 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.AI;
 
-[RequireComponent(typeof(FlashMaterialOnHit))]
 public class EnemyAI : Character, IHearing
 {
     public static List<EnemyAI> Enemies = new();
