5f124b95634e6bfba5e88ca93c877843e88ea514
David Shapiro <daniek332@gmail.com>
2025-07-12 00:07:21 +0300

    Hanged Men WIP

---
+++ Assets/Scripts/Hanged Man/HangedMan.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using UnityEngine;
+
+public class HangedMan : Door
+{
+    [SerializeField] GameObject NoteUI;
+    public override void ChangeState(bool isOpen)
+    {
+        if (isOpen) return;
+        base.ChangeState(isOpen);
+    }
+    public override void Interact()
+    {
+        base.Interact();
+    }
+    protected override void LockedFailedToOpen()
+    {
+        base.LockedFailedToOpen();
+        ShowNote();
+    }
+
+    private void ShowNote()
+    {
+        NoteUI.SetActive(true);
+    }
+}
