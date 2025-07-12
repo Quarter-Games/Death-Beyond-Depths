743357f13d835d881c2968ebdcc788b01d200746
David Shapiro <daniek332@gmail.com>
2025-07-12 13:19:44 +0300

    Update ObjectDissapearer.cs

--- Assets/Scripts/Interactables/ObjectDissapearer.cs
+++ Assets/Scripts/Interactables/ObjectDissapearer.cs
@@ -2,6 +2,7 @@ using UnityEngine;
 
 public class ObjectDissapearer : InteractableObject, IInteractable
 {
+    [SerializeField] bool IsEnabled;
     [SerializeField] GameObject ObjectToDisappear;
     protected override void OnTriggerEnter2D(Collider2D collision)
     {
@@ -18,7 +19,7 @@ public class ObjectDissapearer : InteractableObject, IInteractable
             Debug.LogError("ObjectToDisappear is not set.");
             return;
         }
-        ObjectToDisappear.gameObject.SetActive(false);
+        ObjectToDisappear.gameObject.SetActive(IsEnabled);
     }
 
     public void UnInteract()
