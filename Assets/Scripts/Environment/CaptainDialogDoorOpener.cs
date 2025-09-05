aa1388bc9d7d8a94ceb7e9db69fe586373c8c76e
dendefo <daniek332@gmail.com>
2025-09-05 16:18:37 +0300

    door is opening

---
+++ Assets/Scripts/Environment/CaptainDialogDoorOpener.cs
@@ -0,0 +1,22 @@
+using System;
+using UnityEngine;
+
+public class CaptainDialogDoorOpener : MonoBehaviour
+{
+    [SerializeField] Door door;
+    private void OnEnable()
+    {
+        DialogueController.OnDialogueEnd += UnlockDoor;
+    }
+
+    private void UnlockDoor()
+    {
+        door.Unlock();
+        door.Interact();
+    }
+
+    private void OnDisable()
+    {
+        DialogueController.OnDialogueEnd -= UnlockDoor;
+    }
+}
--- Assets/Scripts/UI/Dialogue Controller.cs
+++ Assets/Scripts/UI/Dialogue Controller.cs
@@ -1,8 +1,11 @@
 using MoreMountains.Feedbacks;
+using System;
+using System.Collections;
 using UnityEngine;
 
 public class DialogueController : MonoBehaviour
 {
+    public static event Action OnDialogueEnd;
     [SerializeField] MMF_Player MMFPlayer;
 
     private void OnValidate()
@@ -12,11 +15,19 @@ public class DialogueController : MonoBehaviour
             MMFPlayer = GetComponent<MMF_Player>();
         }
     }
-
+    private void OnEnable()
+    {
+        StartCoroutine(StartDialogue());
+    }
+    IEnumerator StartDialogue()
+    {
+        yield return MMFPlayer.PlayFeedbacksCoroutine(Vector3.one);
+        OnDialogueEnd?.Invoke();
+    }
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space))
         {
             MMFPlayer.ResumeFeedbacks();
         }
